a375aa9f49bdc1d62b276aa56782d8cb99b72d7f
eduardo-martiniano <eeduardo.martiniano@gmail.com>
2023-03-18 13:57:36 -0300

    add dockerfile e ajustes na tela

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,42 +5,48 @@ import { SpotiService } from 'src/app/services/spoti.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   date = new Date().getFullYear();
-  public convertButtonDisabled = false;
-  public format = 'downloadmp3';
-  public url = "https://www.youtube.com/watch?v=Iy-dJwHVX84&list=RDIy-dJwHVX84&start_radio=1&ab_channel=KendrickLamar-Topic";
-  
-  constructor(private spotiService: SpotiService, private snackBar: MatSnackBar) { }
-  
-  ngOnInit(): void {
-  }
+  converting = false;
+  format = 'downloadmp3';
+  url = 'https://www.youtube.com/watch?v=Iy-dJwHVX84&list=RDIy-dJwHVX84&start_radio=1&ab_channel=KendrickLamar-Topic';
+
+  constructor(
+    private spotiService: SpotiService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
 
   convert() {
-    this.convertButtonDisabled = true;
-    this.spotiService.validate(this.url).then(response => {
+    this.converting = true;
+    this.spotiService.validate(this.url).then((response) => {
       if (response.valid) {
-        this.spotiService.download(this.url, this.format).then(response => {
-          this.convertButtonDisabled = false;
-          this.snackBar.open("Baixando...", "Fechar", {
-            duration: 3 * 1000
+        this.spotiService
+          .download(this.url, this.format)
+          .then((response) => {
+            this.converting = false;
+            this.snackBar.open('Baixando...', 'Fechar', {
+              duration: 3 * 1000,
+            });
+            this.url = '';
+          })
+          .catch((message) => {
+            this.snackBar.open(message, 'Fechar');
+            this.converting = false;
           });
-          this.url = ""; 
-        }).catch((message) => {
-          this.snackBar.open(message, "Fechar");
-          this.convertButtonDisabled = false;
-        });
-      }
-      else {
-        this.snackBar.open("URL invalida", "Fechar");
-        this.url = "";
-        this.convertButtonDisabled = false;
+      } else {
+        this.snackBar.open('URL invalida', 'Fechar');
+        this.url = '';
+        this.converting = false;
       }
     });
-
   }
 
+  buttonDisabled(): Boolean {
+    let empty = this.url.replace(' ', '') == '';
+    return empty || this.converting;
+  }
 }
